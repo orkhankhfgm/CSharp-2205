8b47dd03ff84d9ca0a6737fdcbc33b77369ff408
Orkhan Farajov <orkhankhf@gmail.com>
2022-06-29 22:03:35 +0300

    ArrayList lesson

---
+++ Iyun/29/HashTable-SortedList-Stack-Queue/ArraysHomework/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace ArraysHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Solution 1
+            //Console.WriteLine("Arrayin uzunlugunu yazin: ");
+            //int arrLength = int.Parse(Console.ReadLine());
+
+            //int[] arr = new int[arrLength];
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine("{0}. index: ", i);
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Array dolduruldu!");
+
+            //Console.WriteLine("Arrayin elementlerinin toplami ve ortalama deyeri barede melumat: ");
+            //Console.WriteLine("--------------------------------------------------------------------------");
+
+            //int total = 0, average = 0;
+
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //    total += item;
+            //}
+
+            //Console.WriteLine("--------------------------");
+
+            //Console.WriteLine(total);
+
+            //Console.WriteLine("Arrayin icindeki toplam element sayi: {0}", arr.Length);
+
+            //average = total / arr.Length;
+
+            //Console.WriteLine("Ortalama: {0}", average);
+            //Console.WriteLine("-------------------------");
+            #endregion
+
+
+
+            #region Solution 2
+            //int[] numArr = { 20, 92, 100, 55, 124, 1, 500, 945, 75, 25, 3, 5, 999 };
+            //int temp, j;
+
+            //for (int i = 1; i < numArr.Length; i++)
+            //{
+            //    if(numArr[i] < numArr[i - 1])
+            //    {
+            //        temp = numArr[i];
+
+            //        for (j = i; j > 0 && temp < numArr[j - 1]; j--)
+            //            numArr[j] = numArr[j - 1];
+
+            //        numArr[j] = temp;
+            //    }
+            //}
+
+            //foreach (var item in numArr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+
+
+            #region Solution 3
+            int[] randNums = new int[100];
+            Random rand = new Random();
+
+            int min = 100000;
+            int max = 0;
+            int sum = 0;
+
+            for (int i = 0; i < randNums.Length; i++)
+            {
+                randNums[i] = rand.Next(1, 100000);
+
+                if (randNums[i] > max)
+                    max = randNums[i];
+
+                if (randNums[i] < min)
+                    min = randNums[i];
+
+                sum += randNums[i];
+            }
+
+            Console.WriteLine("Min: " + min);
+            Console.WriteLine("Max: " + max);
+            Console.WriteLine("Sum: " + sum);
+            #endregion
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Iyun/29/HashTable-SortedList-Stack-Queue/HashTable-SortedList-Stack-Queue/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable_SortedList_Stack_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ArrayList
+
+            #region Adding values to ArrayList
+            string[] arr = new string[2];
+            string[] arr2 = new string[]
+            {
+                "One",
+                "Two"
+            };
+
+            ArrayList ArrList = new ArrayList();
+
+            int capacity = 0;
+            int count = 0;
+
+            ArrayList students = new ArrayList();
+            students.Add("Samir");
+            students.Add("Javid");
+            students.Add("Orxan");
+            students.Add("Teymur");
+            students.Add("Gunay");
+            students.Add("Zaur");
+
+            capacity = students.Capacity;
+            count = students.Count;
+
+            students.Add(5);
+            students.Add(true);
+            #endregion
+
+
+
+            #region Adding Collection to ArrayList
+            //A1 sinifi
+            ArrayList A1 = new ArrayList();
+            A1.Add("Samir");
+            A1.Add("Javid");
+            A1.Add("Orxan");
+
+            //A2 sinifi
+            ArrayList A2 = new ArrayList();
+            A2.Add("Teymur");
+            A2.Add("Gunay");
+            A2.Add("Zaur");
+
+            A1.AddRange(A2); //A2'ni A1'e elave edir
+            #endregion
+
+
+
+            #region Getting values from ArrayList
+            var orxan = A1[2]; //object tipindedir
+            object objGunay = A1[4];
+            string strGunay = A1[4].ToString();
+
+            ArrayList mixArray = new ArrayList();
+            mixArray.Add("string value");
+            mixArray.Add(1);
+            mixArray.Add(5.5F);
+            mixArray.Add('C');
+            mixArray.Add(1.2D);
+            mixArray.Add(true);
+
+            var strVal = mixArray[0].ToString();
+            var numVal = (int)mixArray[1];
+            var floatVal = (float)mixArray[2];
+            var charVal = (char)mixArray[3];
+            var doubleVal = (double)mixArray[4];
+            var boolVal = (bool)mixArray[5];
+            #endregion
+
+
+            #region Sorting values in ArrayList
+            //Ters siralama
+            A1.Reverse();
+
+            //A-Z siralama
+            A1.Sort();
+            #endregion
+
+
+            #region Checking if value exists
+            A1.Add("Malik");
+
+            bool ct = A1.Contains("Malik");
+
+            if (A1.Contains("Malik"))
+            {
+                int indexOfName = A1.IndexOf("Malik");
+                A1.RemoveAt(indexOfName);
+            }
+            #endregion
+
+
+
+            #region Clearing (TrimToSize) Array
+            var capacityOfA1 = A1.Capacity;
+            var countOfA1 = A1.Count;
+            A1.Add("Malik");
+            A1.Clear();
+            A1.TrimToSize();
+            #endregion
+            #endregion
+        }
+    }
+}
