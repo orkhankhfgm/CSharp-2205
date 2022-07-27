63403b5e6f91d0598f82354351babd2ce29e74d6
Orkhan Farajov <orkhankhf@gmail.com>
2022-07-27 21:59:56 +0300

    Inner Types

---
+++ Iyul/27/Inner Type/Inner Type/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Inner_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region General
+            Customer customer = new Customer()
+            {
+                Id = 1,
+                Name = "Orkhan",
+                Surname = "Farajov",
+                DocumentNo = 455545,
+                BirthDate = new DateTime(1995, 12, 24),
+                CreateDate = DateTime.Now,
+                EditDate = null
+            };
+
+            Country country = new Country()
+            {
+                Id = 1,
+                Name = "Azerbaijan",
+                CreateDate = DateTime.Now
+            };
+
+            City city = new City()
+            {
+                Id = 1,
+                Name = "Baku",
+                CreateDate = DateTime.Now,
+                Country = country
+            };
+
+            Order laptopOrder = new Order()
+            {
+                Id = 1,
+                OrderDetails = "Asus Tuf fx705gm, 3800",
+                CreateDate = DateTime.Now
+            };
+
+            customer.Addresses[0] = new Address()
+            {
+                Id = 1,
+                No = 15,
+                Building = "MyBuilding",
+                Street = "HomeStreet",
+                Distinct = "Nerimanov",
+                CreateDate = DateTime.Now,
+                City = city
+            };
+
+            customer.Addresses[1] = new Address()
+            {
+                Id = 2,
+                No = 40,
+                Building = "Residance",
+                Street = "WorkStreet",
+                Distinct = "Yasamal",
+                CreateDate = DateTime.Now,
+                City = city
+            };
+
+            customer.Contacts[0] = new Contact()
+            {
+                Id = 1,
+                Email = "orkhankhf@gmail.com",
+                PhoneNumber = "+994555810872",
+                CreateDate = DateTime.Now
+            };
+
+            customer.Orders[0] = new Order()
+            {
+                Id = 1,
+                OrderDetails = "Samsung Galaxy S9+, 500usd",
+                ShippingAddress = customer.Addresses[0],
+                CreateDate = DateTime.Now
+            };
+
+            laptopOrder.ShippingAddress = customer.Addresses[1];
+            customer.Orders[1] = laptopOrder;
+            #endregion
+        }
+    }
+}
---
+++ Iyul/27/Inner Type/Static Homework Solution/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Homework_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            {
+                Id = 1,
+                Name = "Orkhan",
+                Surname = "Farajov",
+                Email = "orkhankhf@gmail.com",
+                Username = "orkhankhf",
+                Password = "orxanDDD1"
+            };
+            Customer.AddCustomer(customer1);
+
+            Customer customer2 = new Customer()
+            {
+                Id = 2,
+                Name = "Elvin",
+                Surname = "Jafarov",
+                Email = "elvin@gmail.com",
+                Username = "elvinj",
+                Password = "elvinDDD1"
+            };
+            Customer.AddCustomer(customer2);
+
+            Customer customer2Duplicate = new Customer()
+            {
+                Id = 2,
+                Name = "Elvin",
+                Surname = "Jafarov",
+                Email = "elvin@gmail.com",
+                Username = "elvinj",
+                Password = "e2"
+            };
+            Customer.AddCustomer(customer2Duplicate);
+        }
+    }
+}
