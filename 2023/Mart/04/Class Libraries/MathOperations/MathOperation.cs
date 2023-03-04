d1335380d36c85d8765be4cb5a76ef8023db20bc
Orkhan Farajov <orkhankhf@gmail.com>
2023-03-04 22:06:46 +0400

    Class libraries and theoretical information about api and web

---
+++ 2023/Mart/04/Class Libraries/MathOperations/MathOperation.cs
@@ -0,0 +1,54 @@
+﻿using MathOperations.Enums;
+using System;
+
+namespace MathOperations
+{
+    public static class MathOperation
+    {
+        public const int MyConstValue = 200;
+
+        #region Public methods
+        public static decimal Calculate(decimal a, decimal b, MathOperationTypeEnum operationType)
+        {
+            switch (operationType)
+            {
+                case MathOperationTypeEnum.Addition:
+                    return Addition(a, b);
+                case MathOperationTypeEnum.Subtraction:
+                    return Subtraction(a, b);
+                case MathOperationTypeEnum.Multiplication:
+                    return Multiplication(a, b);
+                case MathOperationTypeEnum.Divide:
+                    return Divide(a, b);
+                default:
+                    throw new Exception("Səhv əməliyyat növü seçilib!");
+            }
+        }
+        #endregion
+
+        #region Private methods
+        private static decimal Addition(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        private static decimal Subtraction(decimal a, decimal b)
+        {
+            return a - b;
+        }
+
+        private static decimal Multiplication(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        private static decimal Divide(decimal a, decimal b)
+        {
+            if (b == 0)
+                throw new Exception("Ədadi sıfıra bölə bilməzsiniz!");
+
+            return a / b;
+        }
+        #endregion
+    }
+}
---
+++ 2023/Mart/04/SomeProject/SomeProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using MathOperations;
+using MathOperations.Enums;
+using System;
+
+namespace SomeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = MathOperation.Calculate(20, 40, MathOperationTypeEnum.Addition);
+            var x = Convert.ToInt32("15");
+            var c = MathOperation.MyConstValue;
+        }
+    }
+}
